83f22962c67bcd02baa92bcbca12e6bde57a5a45
trong <tranquoctrong1997@gmail.com>
2021-12-22 10:58:24 +0700

    Ket noi voi csdl moi trong database

--- nggame/ketnoisql/ketnoi.php
+++ nggame/ketnoisql/ketnoi.php
@@ -2,7 +2,7 @@
 $dbHost="localhost";
 $dbUser="root";
 $dbPass="";
-$dbName="nggame";
+$dbName="blalbalbalalall";
 $conn=mysqli_connect($dbHost,$dbUser,$dbPass,$dbName);
 if($conn){
     $setLang=mysqli_query($conn,"SET NAMES 'utf8'");
